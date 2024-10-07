ec68e32ff7658dafc70cdcb084a7391ff5f9c8d9
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2024-10-07 00:54:18 -0300

    Perfil dev: sobreescritura de beans

--- src/main/java/ar/edu/iw3/auth/controller/AuthRestController.java
+++ src/main/java/ar/edu/iw3/auth/controller/AuthRestController.java
@@ -46,12 +46,14 @@ public class AuthRestController extends BaseRestController {
 			return new ResponseEntity<>(response.build(HttpStatus.UNAUTHORIZED, e, e.getMessage()),
 					HttpStatus.UNAUTHORIZED);
 		}
+
 		User user = (User) auth.getPrincipal();
 		String token = JWT.create().withSubject(user.getUsername())
 				.withClaim("roles", new ArrayList<String>(user.getAuthoritiesStr())).withClaim("email", user.getEmail())
 				.withClaim("version", "1.0.0")
 				.withExpiresAt(new Date(System.currentTimeMillis() + AuthConstants.EXPIRATION_TIME))
 				.sign(Algorithm.HMAC512(AuthConstants.SECRET.getBytes()));
+
 		return new ResponseEntity<String>(token, HttpStatus.OK);
 	}
 	@Autowired
